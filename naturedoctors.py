70352b74671a5bc647ce2f7cc98691f0d79d94c7
Walachul <walachul@gmail.com>
2019-03-04 09:24:34 +0100

    Removed from the database the email column. Recreated the plants.db

--- naturedoctors.py
+++ naturedoctors.py
@@ -6,32 +6,10 @@ from config import Config
 
 app = Flask(__name__)  #app variable is an instance of the Flask class."__name__" is a special variable._It is just the name of the module._
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-db = SQLAlchemy(app)
 app.config.from_object(Config)
-Plants={
-      'author': 'Username User',
-      'commonName': 'Trandafir',
-      'botanicalName': 'Rosa Regalis',
-      'shortDescription': 'This is a beautiful plant.',
-      'dateAdded': 'April 20, 2021'
-    }
+db = SQLAlchemy(app)
 
-medicinal_use = {
-    'usage':'Stress relief'
-}
 
-Dynamic_Nutrient_Accumulated={
-    'N': 'True',
-    'P': 'True',
-    'K': 'False',
-    'Ca': 'True',
-    'Mg': 'True'
-}
-Nitrogen_Fixers_Nursing={
-    'nursery': 'True',
-    'check_nitrogen': 'False',
-    'comment': 'This plant works best in full sunlight and requires lot of water.'
-}
 
 #Routes
 @app.route("/")
@@ -47,7 +25,7 @@ def statistics():
 @app.route("/about")
 def about():
     return render_template('about.html', title= 'About')
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = UserLoginForm()
     return render_template('login.html', title= 'Login', form=form)
@@ -61,27 +39,26 @@ def register():
 @app.route("/contact")
 def contact():
     return render_template('contact.html', title= 'Contact')
-"""
-#Creating Models for the database
 
-#Plants Table#Users
+#Models for the database
+
+#Users
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default = 'default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default = 'default.png')
     add_plants = db.relationship('Plants', backref = 'author', lazy=True)
 
     def __repr__(self):  #How is the objected printed when we print it out. Thunder/Magic method.
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
+#Plants Table
 class Plants(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     common_name = db.Column(db.String(40), nullable=False)
-    botanical_name = db.Column(db.String(80), nullable=False)
+    botanical_name = db.Column(db.String(80), unique=True, nullable=False)
     short_description = db.Column(db.Text, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default_plant_pic.jpg')
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     medicinal=db.relationship('Medicinal_Use', backref='plant', lazy=True )
@@ -128,6 +105,6 @@ class Nitrogen_Fixers_Nursing(db.Model):
 
     def __repr__(self):
         return f"Nitrogen_Fixers_Nursing('{self.check_nitrogen}', '{self.nursery}', '{self.plant_id}')"
-"""
+
 if __name__ == '__main__':  #The condition is true if we run the script directly.
     app.run(debug=True)
