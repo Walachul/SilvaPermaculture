279ef3b27ef94f1ec8fc88d85724849df270ea9b
Walachul <walachul@gmail.com>
2019-03-07 11:52:16 +0100

    Created the account template and route for it.

--- silvapermaculture/routes.py
+++ silvapermaculture/routes.py
@@ -81,6 +81,11 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route("/account")
+def account():
+
+    return render_template('account.html', title='Account')
+
 
 @app.route("/contact")
 def contact():
