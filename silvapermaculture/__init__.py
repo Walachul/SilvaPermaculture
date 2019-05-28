eea63c6c85f2db4b2424d8caf67936fd25c77e8b
Walachul <walachul@gmail.com>
2019-05-28 18:13:28 +0200

    Implementing Elasticsearch for Full-text search

--- silvapermaculture/__init__.py
+++ silvapermaculture/__init__.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from silvapermaculture.config import Config
-
+from elasticsearch import Elasticsearch
+#Check to see if URL for Esearch is configured. If not, disable Esearch and the app can still run.
+app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
