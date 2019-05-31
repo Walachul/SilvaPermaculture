3f415920e075a0a0558a359433a798b94b44a7f3
Walachul <walachul@gmail.com>
2019-05-31 16:42:27 +0200

    Restyled whole plants template to better display the plants. Images are now resized with Flask to 280x180 px>'

--- silvapermaculture/routes.py
+++ silvapermaculture/routes.py
@@ -105,7 +105,7 @@ def save_plant_picture(form_plantPic):
     _, f_ext = os.path.splitext(form_plantPic.filename)
     picture_fn = random_hex_image + f_ext
     picture_path = os.path.join(app.root_path, 'static/img/plants', picture_fn) #Saving the new plantPic to the specified folder.
-    scale_image = (250, 125)
+    scale_image = (286, 180)
     img_new = Image.open(form_plantPic)
     img_new.thumbnail(scale_image)
     img_new.save(picture_path)
