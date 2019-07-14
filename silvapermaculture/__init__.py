8f97ee319fee3cde32bc974123200e62945e28ab
Walachul <walachul@gmail.com>
2019-07-14 22:32:35 +0200

    heroku deployment changes

--- silvapermaculture/__init__.py
+++ silvapermaculture/__init__.py
@@ -15,5 +15,24 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+#Heroku deploymenet
+
+if app.config['LOG_TO_STDOUT']:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
+else:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/silvapermaculture.log',
+                                       maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('Silvapermaculture startup')
 
 from silvapermaculture import routes #This is imported here to avoid circular imports.
