04a0c9df28c831666dc473db5553edc8491303d8
Walachul <walachul@gmail.com>
2019-07-21 17:52:36 +0200

    Moved the creation of the application inside a function.

--- silvapermaculture/__init__.py
+++ silvapermaculture/__init__.py
@@ -5,21 +5,30 @@ from flask_login import LoginManager
 from silvapermaculture.config import Config
 from elasticsearch import Elasticsearch
 
-app = Flask(__name__)
-app.config.from_object(Config)
-#Check to see if URL for Esearch is configured. If not, disable Esearch and the app can still run.
-app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-    if app.config['ELASTICSEARCH_URL'] else None
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from silvapermaculture.users.routes import users #This is imported here to avoid circular imports.
-from silvapermaculture.plants.routes import plants #This is imported here to avoid circular imports.
-from silvapermaculture.main.routes import main #This is imported here to avoid circular imports.
 
-app.register_blueprint(users)
-app.register_blueprint(plants)
-app.register_blueprint(main)
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    # Check to see if URL for Esearch is configured. If not, disable Esearch and the app can still run.
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
+    from silvapermaculture.users.routes import users  # This is imported here to avoid circular imports.
+    from silvapermaculture.plants.routes import plants  # This is imported here to avoid circular imports.
+    from silvapermaculture.main.routes import main  # This is imported here to avoid circular imports.
+    app.register_blueprint(users)
+    app.register_blueprint(plants)
+    app.register_blueprint(main)
+
+    return app
