307c49924347f6a4214568c32a6a8d879c16a33e
Walachul <walachul@gmail.com>
2019-07-23 21:00:32 +0200

    Modified the environmental variable to DATABASE_URL to better represent the variable on Heroku.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     PLANTS_PER_PAGE = 5
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
