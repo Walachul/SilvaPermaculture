0b7671cdca0de775a6e2c7fc3b4fb3fb1453c85d
Walachul <walachul@gmail.com>
2019-07-27 20:30:04 +0200

    removed decode function from random_hex_image due to heroku error

--- silvapermaculture/plants/utilities.py
+++ silvapermaculture/plants/utilities.py
@@ -9,7 +9,7 @@ def save_plant_picture(form_plantPic):
     Change img filename to be a random hex, instead of keeping the original name of the file,
     in order to avoid having files with the same name.
     """
-    random_hex_image = secrets.token_hex(8).decode("utf-8", "ignore")
+    random_hex_image = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_plantPic.filename)
     picture_fn = random_hex_image + f_ext
     picture_path = os.path.join(current_app.root_path, 'static/img/plants', picture_fn) #Saving the new plantPic to the specified folder.
